18109554f074caf51d2760d0a0e2c51173ee91aa
nhungoc-2907 <ngocltn23411@st.uel.edu.vn>
2026-06-12 01:50:14 +0700

    Add checkout and QR payment flow

--- app/src/main/java/com/veggo/app/MainActivity.java
+++ app/src/main/java/com/veggo/app/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.veggo.app.databinding.ActivityMainBinding;
+import com.veggo.app.presentation.cart.CartFragment;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -14,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.nav_cart) {
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.mainFragmentContainer, new CartFragment())
+                        .commit();
+
+                return true;
+            }
+
+            return false;
+        });
+
+
     }
 }
